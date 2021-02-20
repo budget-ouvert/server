b52f25a3edd2139b2d3bb2e71def48aae078489f
alexis <alexisthual@gmail.com>
2021-02-20 17:57:06 +0100

    Switch from using waitress to gunicorn

--- main.py
+++ main.py
@@ -4,20 +4,15 @@ from flask_cors import CORS
 
 from api import NodeHistory, FileHandler
 
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    api = Api(app)
-
-    api.add_resource(
-        NodeHistory, "/node_history/<string:source>/<int:year>/<string:node_id>"
-    )
-    api.add_resource(FileHandler, "/hierarchy/<string:source>/<int:year>")
-
-    return app
+api.add_resource(
+    NodeHistory, "/node_history/<string:source>/<int:year>/<string:node_id>"
+)
+api.add_resource(FileHandler, "/hierarchy/<string:source>/<int:year>")
 
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
